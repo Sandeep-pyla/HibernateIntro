238ea62b3cfc0facfefe7cb13146a7b8de7ecc43
Sandeep-pyla <sp3288@njit.edu>
2026-06-04 14:07:50 -0500

    updated hibernate code

---
+++ src/main/java/com/project/HibernateIntro/HibernateUtil.java
@@ -0,0 +1,16 @@
+package com.project.HibernateIntro;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Book.class).buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
